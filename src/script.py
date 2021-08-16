55a438305dea6010bf2cea35c8ec47eba8db7f45
draabe <draabe@posteo.de>
2021-08-16 14:07:45 +0200

    initial commit

---
+++ src/script.py
@@ -0,0 +1,12 @@
+
+
+a = {1:'eins', 2:'zwei'}
+
+print(f"This is very crappy code. It does not look good. Lorem Ipsum. I'm running out of words here. What is this? It's a {a[1]}!!!!")
+
+
+for i in a:
+    if True:
+        print(
+            i
+        )
